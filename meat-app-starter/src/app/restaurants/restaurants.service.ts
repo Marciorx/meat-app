4c0e9e77f938439b137dff73668d5380c3e09eeb
Marciorx <marcio.mag@outlook.com>
2018-09-24 17:32:32 -0300

    Shopping Cart

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -8,6 +8,7 @@ import {MEAT_API} from '../app.api'
 
 import {Observable} from 'rxjs/Observable'
 import {ErrorHandler} from '../app.error-handler'
+import { MenuItem } from '../restaurant-detail/menu-item/menu-item.model';
 
 @Injectable()
 export class RestaurantsService {
@@ -22,6 +23,18 @@ export class RestaurantsService {
 
     restaurantByID(id: string): Observable<Restaurant>{
         return this.http.get(`${MEAT_API}/restaurants/${id}`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurant(id: string): Observable<any>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurant(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
           .map(response => response.json())
           .catch(ErrorHandler.handleError)
     }
