b5341c26e5b191c2d7edc4f2cf08595258731976
Marciorx <marcio.mag@outlook.com>
2018-10-01 17:22:41 -0300

    aula 65

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -20,7 +20,9 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart-service';
 import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component'
+import { InputComponent } from './shared/input/input.component';
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItemsComponent } from './order/src/app/order/order-items/order-items.component'
 
 
 @NgModule({
@@ -37,7 +39,9 @@ import { InputComponent } from './shared/input/input.component'
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent
+    InputComponent,
+    RadioComponent,
+    OrderItemsComponent
     
   ],
   imports: [
