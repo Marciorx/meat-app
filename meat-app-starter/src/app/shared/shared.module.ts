24fa0a16548ae9a5100030ef565ac4ad68b0e914
Marcio Magalhaes <marcio.mag@outlook.com>
2018-11-06 00:27:09 -0300

    Comecando aula 120

--- meat-app-starter/src/app/shared/shared.module.ts
+++ meat-app-starter/src/app/shared/shared.module.ts
@@ -13,6 +13,8 @@ import { SnackbarComponent } from './messages/snackbar/snackbar.component';
 
 import {NotificationService} from './messages/notification.service';
 
+import {LoginService} from '../security/login/login.service'
+
 
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -25,7 +27,7 @@ export class SharedModule {
     static forRoot(): ModuleWithProviders {
         return {
             ngModule: SharedModule,
-            providers: [ShoppingCartService, RestaurantsService, OrderService, NotificationService]
+            providers: [ShoppingCartService, RestaurantsService, OrderService, NotificationService, LoginService]
 
         }
     }
